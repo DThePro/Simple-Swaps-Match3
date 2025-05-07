557e7b144f40288d6446d161ed6d726389c5b426
DThePro <deeprochakravorty22@gmail.com>
2025-05-08 00:50:12 +0530

    Added Bomb Asset

--- Assets/Scripts/ItemDatabase.cs
+++ Assets/Scripts/ItemDatabase.cs
@@ -4,11 +4,13 @@ public static class ItemDatabase
 {
     public static Item[] Items { get; private set; }
     public static Item[] SpecialItems { get; private set; }
+    public static Item[] EnemyItems { get; private set; }
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void Initialize()
     {
         Items = Resources.LoadAll<Item>("Items/");
         SpecialItems = Resources.LoadAll<Item>("Special/");
+        EnemyItems = Resources.LoadAll<Item>("Enemies/");
     }
 }
